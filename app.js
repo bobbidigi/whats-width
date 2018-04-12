3918638fe847888ab2544c16e0a39158a7f7e39a
bobbidigi <bobbidigi34@gmail.com>
2018-04-12 19:33:43 -0400

    add content, add code hints w/ refactoring

--- app.js
+++ app.js
@@ -1,117 +1,140 @@
-//score
-var pts    = document.getElementById('pts');
-//top buttons
-var resetButton   = document.querySelector("#reset");
-var contentBoxBtn = document.getElementById('contentBox');
-var borderBoxBtn  = document.getElementById('borderBox');
-//guessWidth element
-var guessWidth  = document.getElementById('guessWidth');
-var borderBoxCode = document.querySelector('.code-one');
-//css display css css css
-var boxSizing   = document.getElementById('box-sizing');
-var width   = document.getElementById('width');
-var margin  = document.getElementById('margin');
-var padding = document.getElementById('padding');
-var border  = document.getElementById('border');
-//answer buttons
-var ansBtn0 = document.getElementById('answer0');
-var ansBtn1 = document.getElementById('answer1');
-var ansBtn2 = document.getElementById('answer2');
-// var hide = document.querySelector('.none');
-var boxBoolean = false;
-var bs = 'content-box';
-var CSS;
-var points = 0;
+// eliminate comments
+// size and space things appropriately
+// use descriptive variable names
+// get elements by ids when possible
+// install eslint to help with all this
+// dont name variables the same as your global variables
+// always use stictly equals ===
+// never use var
+// almost always use arrow functions
+// use innerText instead of innerHTML if you are just passign a string in
+// only 1 line at the end of a file
+// smaller functions
+// be consistent with variable naming
+// ESLint or Prettier would help with that (for example the extra semicolon on line 64). 
+//be consistent in usage of ES6
+//there's an unnecessary mix of arrow functions and regular functions. 
+//I would also suggest to use let instead of var, cause it's safer.
 
-//start game from dom loaded
-window.addEventListener('DOMContentLoaded',reset);
-//new width btn reset all
-resetButton.addEventListener("click",reset);
-contentBoxBtn.addEventListener("click",boxSizingProp);
-borderBoxBtn.addEventListener("click",boxSizingProp);
-//adds css to selected button and changes answers and box-sizing 
-function boxSizingProp(){
-  //if borderBox is true/ btn press
- if(this.id == 'borderBox'){
-     boxBoolean = true;
-     bs = 'border-box';
-     this.classList.add('selected');
-     contentBoxBtn.classList.remove('selected');
-     borderBoxCode.style.display = 'block';
- }else{
-     //contentBox is true/ btn press
-     boxBoolean = false;
-     bs =  'content-box';
-     this.classList.add('selected');
-     borderBoxBtn.classList.remove('selected');
-     borderBoxCode.style.display = 'none';
+const score = document.getElementById('pts');
+const resetButton = document.getElementById('reset');
+const contentBoxBtn = document.getElementById('contentBox');
+const borderBoxBtn = document.getElementById('borderBox');
+
+const guessWidth = document.getElementById('guessWidth');
+const borderBoxCode = document.querySelector('.code-one');
+//Better names needed here
+const boxSizing = document.getElementById('box-sizing');
+const width = document.getElementById('width');
+const margin = document.getElementById('margin');
+const padding = document.getElementById('padding');
+const border = document.getElementById('border');
+
+const answerButton0 = document.getElementById('answer0');
+const answerButton1 = document.getElementById('answer1');
+const answerButton2 = document.getElementById('answer2');
+
+let boxBoolean = false;
+let bs = 'content-box';
+let CSS;
+let points = 0;
+//autofire this without a function name
+function onLoad() {
+  window.addEventListener('DOMContentLoaded', reset);
+  resetButton.addEventListener('click', reset);
+  contentBoxBtn.addEventListener('click', boxSizingProp);
+  borderBoxBtn.addEventListener('click', boxSizingProp);
+}
+onLoad();
+
+function boxSizingProp() {
+ if (this.id === 'borderBox') {
+    console.log(boxBoolean);
+    console.log(bs);
+    sizeBorderBox();
+ } else {
+    sizeContentBox();
  } 
  reset();
 }
 
-function addPoints(i){
-  points += i;
-  pts.innerHTML = points;
-  greatJob();      
-}
-//fade-in-and-out greatJob!
-function greatJob(){
-   var elem = document.getElementById('great');
-   elem.style.transition =" opacity .5s linear 0s";
-   elem.style.opacity = 1;    
-   setTimeout(function(){
-   elem.style.opacity = 0;           
-   ;}, 3000);    
+function sizeBorderBox() {
+  boxBoolean = true;
+  bs = 'border-box';
+  borderBoxBtn.classList.add('selected');
+  contentBoxBtn.classList.remove('selected');
+  borderBoxCode.style.display = 'block';
 }
 
-//create a random number function 
-function randomNumber(min,max){
-  return Math.floor(Math.random() * (max - min + 1)+ min );
+function sizeContentBox() {
+  boxBoolean = false;
+  bs = 'content-box';
+  contentBoxBtn.classList.add('selected');
+  borderBoxBtn.classList.remove('selected');
+  borderBoxCode.style.display = 'none';
 }
-// give the guessWidth element random styles
+
+function addPoints(point) {
+  points += point;
+  score.innerText = points;
+  greatJobAnimation();      
+}
+
+function greatJobAnimation() {
+   const greatJob = document.getElementById('great');
+   greatJob.style.transition = 'opacity .5s linear 0s';
+   greatJob.style.opacity = 1;    
+   setTimeout(() => { greatJob.style.opacity = 0; }, 3000); 
+}
+
+function randomNumber(min, max) {
+  return Math.floor((Math.random() * ((max - min) + 1)) + min);
+}
+
 function setStyles() {
-  var width  = randomNumber(1,7);
-  var pad    = randomNumber(1,3);
-  var brdr   = randomNumber(1,10);
-  var mrgn   = randomNumber(1,10);
-  var bstyle = bs.toString();
-  CSS = [width, pad, mrgn, brdr];
+  const width2 = randomNumber(1, 7);
+  const pad = randomNumber(1, 3);
+  const brdr = randomNumber(1, 10);
+  const mrgn = randomNumber(1, 10);
+  const bstyle = bs.toString();
+  CSS = [width2, pad, mrgn, brdr];
+  
   guessWidth.style.boxSizing = `${bstyle}`;    
-  guessWidth.style.width     = `${width}00px`;
-  guessWidth.style.padding   = `${pad}0px`;
-  guessWidth.style.border    = `${brdr}px solid pink`;
-  guessWidth.style.margin    = `${mrgn}px`; 
+  guessWidth.style.width = `${width2}00px`;
+  guessWidth.style.padding = `${pad}0px`;
+  guessWidth.style.border = `${brdr}px solid pink`;
+  guessWidth.style.margin = `${mrgn}px`; 
 }
-//displays css properties in code box
-function cssDisplay(){
-  boxSizing.innerHTML  = guessWidth.style.boxSizing;    
-  width.innerHTML      = guessWidth.style.width;
-  padding.innerHTML    = guessWidth.style.padding;
-  margin.innerHTML     = guessWidth.style.margin;
-  border.innerHTML     = guessWidth.style.border;    
-} 
 
-function answerBtns(){
-  // Random answers 
-  var correct;
-  // diff answers depending on whether or not boxBoolean is true    
-  // if border box is true correct answer
-  if(boxBoolean == true){
-    correct  = (CSS[0]*100)+(CSS[2]*2);
-  }else{
-    correct = (CSS[0]*100)+((CSS[1]*10)*2)+(CSS[2]*2)+(CSS[3]*2);
+function cssDisplayCodeBox() {
+  boxSizing.innerHTML = guessWidth.style.boxSizing;    
+  width.innerHTML = guessWidth.style.width;
+  padding.innerHTML = guessWidth.style.padding;
+  margin.innerHTML = guessWidth.style.margin;
+  border.innerHTML = guessWidth.style.border;    
+} 
+// break this out in to different functions
+function answerBtns() {
+  let correctAnswer;
+  const incorrectAnswer1 = (CSS[0] * 100);
+  
+  if (boxBoolean === true) {
+    correctAnswer = (CSS[0] * 100) + (CSS[2] * 2);
+  } else {
+    correctAnswer = (CSS[0] * 100) + ((CSS[1] * 10) * 2) + (CSS[2] * 2) + (CSS[3] * 2);
   }
-  var incorrect1 = (CSS[0]*100);
-  var incorrect2 = correct - (CSS[3]*2);
-  var answers    = [correct, incorrect1, incorrect2];
-  //sort the answers
-  answers.sort((a,b) => 0.5 - Math.random());
-  var ansButtons = [ansBtn0, ansBtn1, ansBtn2];
-  ansButtons.forEach((btn,i) => {
+  
+  const incorrectAnswer2 = correctAnswer - (CSS[3] * 2);
+  const answers = [correctAnswer, incorrectAnswer1, incorrectAnswer2];
+
+  answers.sort((a, b) => a < b);
+
+  const ansButtons = [answerButton0, answerButton1, answerButton2];
+  ansButtons.forEach((btn, i) => {
     btn.innerHTML = answers[i];
-    //add event listener 
-    btn.addEventListener('click',e => {
-      checkResult(e,correct);
+  
+    btn.addEventListener('click', e => {
+      checkResult(e, correctAnswer);
     });
   });
 }
@@ -120,7 +143,7 @@ const resultDisplay = document.querySelector('.result');
 resultDisplay,addEventListener('click', e => {
   if( e.target.id === 'reset-won'){
   setStyles();
-  cssDisplay(); 
+  cssDisplayCodeBox(); 
   answerBtns(); 
   resultDisplay.style.display = 'none';
   }
@@ -128,6 +151,7 @@ resultDisplay,addEventListener('click', e => {
   return false;
   }
 });
+
 //check Result 
 function checkResult(e,answer){
   let currentNumber = Number(e.target.textContent);
@@ -141,7 +165,7 @@ function checkResult(e,answer){
                                   <button id='reset-won'>PLAY AGAIN </button>`;
        setTimeout(function(){
          points = 0;
-         pts.innerHTML = '0'; }, 3000);
+         score.innerHTML = '0'; }, 3000);
  
   }else{
         resultDisplay.style.display = 'none';
@@ -152,9 +176,7 @@ function checkResult(e,answer){
 // reset the game 
 function reset(){
   setStyles();
-  cssDisplay(); 
+  cssDisplayCodeBox(); 
   answerBtns();
   // resultDisplay.style.display = none;
 }
-
-
